b162e9c023c7869a4af208f1ade3e579f76d4894
DenisLobur <denisdenisdenis@bigmir.net>
2018-10-12 20:03:45 +0300

    add more roots wariants

--- RootVariants.hs
+++ RootVariants.hs
@@ -24,4 +24,12 @@ roots''' a b c =
         x2 = (-b + d) / aTwice
         d = sqrt(b ^ 2 - 4 * a * c)
         aTwice = 2 * a
-    in (x1, x2)                             
+    in (x1, x2)    
+    
+roots'''' a b c = (x1, x2)
+    where
+        x1 = (-b - d) / aTwice
+        x2 = (-b + d) / aTwice
+        d = sqrt(b ^ 2 - 4 * a * c)
+        aTwice = 2 * a    
+                                 
